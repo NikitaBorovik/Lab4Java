dc38bde140f2af1c1ab13df456f84630ee83ff8a
Нікіта Боровік <nikita.borovik@ukma.edu.ua>
2023-06-22 17:32:31 +0300

    testing CI/CD

--- PortScanner/src/test/java/Tests.java
+++ PortScanner/src/test/java/Tests.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class Tests {
     @Test
     public void assertThreadsNumber(){
-        //asdas
+        //test
         PortScanner portScanner = new PortScanner();
         assertTrue(portScanner.getTHREADS_NUM() <= 16 && portScanner.getTHREADS_NUM() > 0);
     }
