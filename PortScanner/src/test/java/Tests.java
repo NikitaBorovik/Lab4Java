4b0160536f502c1bc77d74f3799289c27a331fd7
Нікіта Боровік <nikita.borovik@ukma.edu.ua>
2023-06-22 14:53:44 +0300

    test commit

--- PortScanner/src/test/java/Tests.java
+++ PortScanner/src/test/java/Tests.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class Tests {
     @Test
     public void assertThreadsNumber(){
+        //asdas
         PortScanner portScanner = new PortScanner();
         assertTrue(portScanner.getTHREADS_NUM() <= 16 && portScanner.getTHREADS_NUM() > 0);
     }
